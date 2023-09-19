332a7a8972fd5b631c572ec0778d3183d5da5f67
action <noop@github.com>
2023-09-19 02:19:40 +0000

    Pages 33

--- web/main.dart.js
+++ web/main.dart.js
@@ -23578,7 +23578,7 @@ $1(a){var s=$.bL
 s=(s==null?$.bL=A.e9(self.window.flutterConfiguration):s).b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/b20183e04096094bcc37d9cde2a4b96f5cc684cf/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/9064459a8b0dcd32877107f6002cc429a71659d1/":s)+a},
 $S:104}
 A.adt.prototype={
 $1(a){this.a.remove()
