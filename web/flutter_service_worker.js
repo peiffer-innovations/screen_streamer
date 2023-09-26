243bf15b836bf6edf78d1583223ca8f65137ee6d
action <noop@github.com>
2023-09-26 02:20:20 +0000

    Pages 34

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -22,10 +22,10 @@ const RESOURCES = {"canvaskit/chromium/canvaskit.js": "96ae916cd2d1b7320fff853ee
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
-"index.html": "016311f0d3685375b84628e68318acef",
-"/": "016311f0d3685375b84628e68318acef",
+"index.html": "9d4370079a6f0803a56f63902d84c747",
+"/": "9d4370079a6f0803a56f63902d84c747",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "13baa3ce3145e0bebf536d9a05d18d35",
+"main.dart.js": "7fe5a91f1932f27b7844120a85c833ad",
 "manifest.json": "321676efc8042eed844ac77888625f52"};
 // The application shell files that are downloaded before a service worker can
 // start.
--- web/main.dart.js
+++ web/main.dart.js
@@ -23578,7 +23578,7 @@ $1(a){var s=$.bL
 s=(s==null?$.bL=A.e9(self.window.flutterConfiguration):s).b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/9064459a8b0dcd32877107f6002cc429a71659d1/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/bd986c5ed20a62dc34b7718c50abc782beae4c33/":s)+a},
 $S:104}
 A.adt.prototype={
 $1(a){this.a.remove()
